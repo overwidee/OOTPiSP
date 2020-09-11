5ef911a4055c168d07db2e59c284f7e10b616f43
the-shichko <pashashichko0075@gmail.com>
2020-09-11 19:28:53 +0300

    Add project files.

---
+++ example1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            Console.WriteLine("example1");
+
+            double a, b, c;
+
+            Console.WriteLine("Print a: ");
+            double.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Print b: ");
+            double.TryParse(Console.ReadLine(), out b);
+            Console.WriteLine("Print c: ");
+            double.TryParse(Console.ReadLine(), out c);
+
+            Console.WriteLine((a > Math.Abs(b - c) && (a < b + c)) ? "Can" : "Cannot");
+            #endregion
+
+            #region Task 2
+
+            #endregion
+
+
+        }
+    }
+}
---
+++ example1_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace example1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("example1_1");
+        }
+    }
+}
