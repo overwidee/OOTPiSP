47ac6f532ee828d317a8c8f19faeb0a4453b43b8
the-shichko <pashashichko0075@gmail.com>
2020-09-11 20:05:18 +0300

    [add] example1

--- example1/Program.cs
+++ example1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace example1
 {
@@ -7,25 +8,71 @@ namespace example1
         static void Main(string[] args)
         {
             #region Task 1
-            Console.WriteLine("example1");
+            //Console.WriteLine("example1");
 
-            double a, b, c;
+            //double a, b, c;
 
-            Console.WriteLine("Print a: ");
-            double.TryParse(Console.ReadLine(), out a);
-            Console.WriteLine("Print b: ");
-            double.TryParse(Console.ReadLine(), out b);
-            Console.WriteLine("Print c: ");
-            double.TryParse(Console.ReadLine(), out c);
+            //Console.WriteLine("Print a: ");
+            //double.TryParse(Console.ReadLine(), out a);
+            //Console.WriteLine("Print b: ");
+            //double.TryParse(Console.ReadLine(), out b);
+            //Console.WriteLine("Print c: ");
+            //double.TryParse(Console.ReadLine(), out c);
 
-            Console.WriteLine((a > Math.Abs(b - c) && (a < b + c)) ? "Can" : "Cannot");
+            //Console.WriteLine((a > Math.Abs(b - c) && (a < b + c)) ? "Can" : "Cannot");
             #endregion
 
             #region Task 2
 
+            //int n, m;
+            //Console.WriteLine("Print n: ");
+            //int.TryParse(Console.ReadLine(), out n);
+
+            //Console.WriteLine("Print m: ");
+            //int.TryParse(Console.ReadLine(), out m);
+
+            //// Рекурсия
+            //Console.WriteLine($"(R) A(n, m) = {Fact(n) / (Fact(n - m))}");
+            //// Loop
+            //Console.WriteLine($"(L) A(n, m) = {Loop(n, m)}");
             #endregion
 
+            #region Task 3
 
+            Console.WriteLine("Print accuracy: ");
+            long.TryParse(Console.ReadLine(), out var acc);
+            acc = long.MaxValue;
+
+            long i = 1;
+            double pi = 0;
+            long x = 1;
+            while (i != acc)
+            {
+                pi += 1.0 / (x * (x + 2));
+                x += 4;
+
+                Console.WriteLine($"Pi = {pi * 8}");
+                i++;
+            }
+
+
+            #endregion
+        }
+
+        static int Fact(int x)
+        {
+            return x != 1 ? Fact(x - 1) * x : 1;
+        }
+
+        static int Loop(int x, int y)
+        {
+            var z = 1;
+            for (var n = x - y + 1; n <= x; n++)
+            {
+                z *= n;
+            }
+
+            return z;
         }
     }
 }
