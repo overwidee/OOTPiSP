c2d5b9694cc768f848b4cc9241f192dcdeb35a42
the-shichko <pashashichko0075@gmail.com>
2020-12-14 22:19:19 +0300

    [feat] task 2

--- example6/Program.cs
+++ example6/Program.cs
@@ -1,4 +1,7 @@
-﻿using static System.Console;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
 
 namespace example6
 {
@@ -6,16 +9,43 @@ namespace example6
     {
         static void Main(string[] args)
         {
-            var number1 = new Rational(1, 2);
-            var number2 = new Rational(3, 4);
-            
-            WriteLine($"number1 = {number1}");
-            WriteLine($"number2 = {number2}\n");
+            #region Task 1
 
-            WriteLine($"number1 * number2 = {number1.Multiply(number2)}");
-            WriteLine($"number1 / number2 = {number1.Divide(number2)}");
-            WriteLine($"number1 + number2 = {number1.Add(number2)}");
-            WriteLine($"number1 - number2 = {number1.Subtract(number2)}");
+            // var number1 = new Rational(1, 2);
+            // var number2 = new Rational(3, 4);
+            //
+            // WriteLine($"number1 = {number1}");
+            // WriteLine($"number2 = {number2}\n");
+            //
+            // WriteLine($"number1 * number2 = {number1.Multiply(number2)}");
+            // WriteLine($"number1 / number2 = {number1.Divide(number2)}");
+            // WriteLine($"number1 + number2 = {number1.Add(number2)}");
+            // WriteLine($"number1 - number2 = {number1.Subtract(number2)}");
+
+            #endregion
+
+            #region Task 2
+
+            var incomes = new List<Income>
+            {
+                new MainWork(1000), 
+                new SecondWork(500), 
+                new Royalty(350),
+                new Sales(998),
+                new Gift(3354),
+                new Transfer(3300),
+                new Privilege(445)
+            };
+
+            var sortList = incomes.OrderByDescending(x => ((ITax) x).GetTaxSum()).ToList();
+
+            foreach (var income in sortList)
+            {
+                WriteLine($"{income.GetName()} - {Math.Round(((ITax)income).GetTaxSum(), 2)}");
+            }
+
+            #endregion
+
             ReadLine();
         }
     }
---
+++ example6/Tax.cs
@@ -0,0 +1,111 @@
+﻿namespace example6
+{
+    public interface ITax
+    {
+        double GetTaxSum();
+    }
+
+    public class Income
+    {
+        protected string _name;
+        private readonly double _value;
+
+        protected Income(double value)
+        {
+            _value = value;
+        }
+
+        protected double GetMoney() => _value;
+        public string GetName() => _name;
+    }
+    
+    public class MainWork : Income, ITax
+    {
+        public MainWork(double value) : base(value)
+        {
+            _name = "Основная работа";
+        }
+
+        public double GetTaxSum()
+        {
+            return GetMoney() * 0.25;
+        }
+    }
+    public class SecondWork : Income, ITax
+    {
+        public SecondWork(double value) : base(value)
+        {
+            _name = "Доп. работа";
+        }
+
+        public double GetTaxSum()
+        {
+            return GetMoney() * 0.15;
+        }
+    }
+
+    public class Royalty : Income, ITax
+    {
+        public Royalty(double value) : base(value)
+        {
+            _name = "Авторские вознаграждения";
+        }
+
+        public double GetTaxSum()
+        {
+            return GetMoney() * 0.17;
+        }
+    }
+
+    public class Sales : Income, ITax
+    {
+        public Sales(double value) : base(value)
+        {
+            _name = "Продажи";
+        }
+
+        public double GetTaxSum()
+        {
+            return GetMoney() * 0.09;
+        }
+    }
+
+    public class Gift : Income, ITax
+    {
+        public Gift(double value) : base(value)
+        {
+            _name = "Подарки денежных сумм и имущества";
+        }
+
+        public double GetTaxSum()
+        {
+            return GetMoney() * 0.3;
+        }
+    }
+
+    public class Transfer : Income, ITax
+    {
+        public Transfer(double value) : base(value)
+        {
+            _name = "Переводов из-за границы";
+        }
+
+        public double GetTaxSum()
+        {
+            return GetMoney() * 0.23;
+        }
+    }
+
+    public class Privilege : Income, ITax
+    {
+        public Privilege(double value) : base(value)
+        {
+            _name = "Льгот на детей и материальной помощи";
+        }
+
+        public double GetTaxSum()
+        {
+            return GetMoney() * 0.2;
+        }
+    }
+}
