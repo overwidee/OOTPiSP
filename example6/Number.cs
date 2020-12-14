0ff148f6857291a2573d986a998f25ece793a533
the-shichko <pashashichko0075@gmail.com>
2020-12-14 21:32:10 +0300

    [feat] add example6 first task

---
+++ example6/Number.cs
@@ -0,0 +1,58 @@
+﻿namespace example6
+{
+    public interface INumber
+    {
+        INumber Add(INumber number);
+        INumber Subtract(INumber number);
+        INumber Multiply(INumber number);
+        INumber Divide(INumber number);
+    }
+
+    public class Rational : INumber
+    {
+        private readonly int _numerator;
+        private readonly int _denominator;
+
+        public Rational(int numerator, int denominator)
+        {
+            _numerator = numerator;
+            _denominator = denominator;
+        }
+
+        public int GetX() => _numerator;
+        public int GetY() => _denominator;
+
+        public INumber Add(INumber number)
+        {
+            var n = ((Rational) number).GetX();
+            var d = ((Rational) number).GetY();
+            return new Rational(_numerator * d + _denominator * n,
+                _denominator * d);
+        }
+
+        public INumber Subtract(INumber number)
+        {
+            var n = ((Rational) number).GetX();
+            var d = ((Rational) number).GetY();
+            return new Rational(_numerator * d - _denominator * n,
+                _denominator * d);
+        }
+
+        public INumber Multiply(INumber number)
+        {
+            return new Rational(_numerator * ((Rational) number).GetX(),
+                _denominator * ((Rational) number).GetY());
+        }
+
+        public INumber Divide(INumber number)
+        {
+            return new Rational(_numerator * ((Rational) number).GetY(),
+                _denominator * ((Rational) number).GetX());
+        }
+
+        public override string ToString()
+        {
+            return $"{_numerator}/{_denominator}";
+        }
+    }
+}
---
+++ example6/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+namespace example6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number1 = new Rational(1, 2);
+            var number2 = new Rational(3, 4);
+            
+            WriteLine($"number1 = {number1}");
+            WriteLine($"number2 = {number2}\n");
+
+            WriteLine($"number1 * number2 = {number1.Multiply(number2)}");
+            WriteLine($"number1 / number2 = {number1.Divide(number2)}");
+            WriteLine($"number1 + number2 = {number1.Add(number2)}");
+            WriteLine($"number1 - number2 = {number1.Subtract(number2)}");
+            ReadLine();
+        }
+    }
+}
