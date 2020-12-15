b2a90c747906d475a6b79b6f8de7acddbd2c0c11
the-shichko <pashashichko0075@gmail.com>
2020-12-15 20:14:58 +0300

    [feat] example8

---
+++ example8/Models/IPrintingDevice.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace example8.Models
+{
+    public interface IPrintingDevice
+    {
+        void Print();
+        void RefillCartridge();
+    }
+
+    public class Printer : BasePrinter, IPrintingDevice
+    {
+        public void Print()
+        {
+            Console.WriteLine("Печатает обычный принтер");
+        }
+
+        public void RefillCartridge()
+        {
+            Console.WriteLine("Заправляем обычный принтер");
+        }
+
+        public Printer(int countList) : base(countList)
+        {
+        }
+    }
+
+    public class LaserPrinter : BasePrinter, IPrintingDevice
+    {
+        public void Print()
+        {
+            Console.WriteLine("Печатает лазерный принтер");
+        }
+
+        public void RefillCartridge()
+        {
+            Console.WriteLine("Заправляем лазерный принтер");
+        }
+
+        public LaserPrinter(int countList) : base(countList)
+        {
+        }
+    }
+
+    public abstract class BasePrinter
+    {
+        protected int _countList;
+
+        protected BasePrinter(int countList)
+        {
+            _countList = countList;
+        }
+
+        public int GetCountLists() => _countList;
+    }
+    
+    public class Technics : IEnumerable<IPrintingDevice>
+    {
+        private List<IPrintingDevice> _devices = new List<IPrintingDevice>();
+
+        public Technics(IEnumerable<IPrintingDevice> devices)
+        {
+            _devices.AddRange(devices);
+        }
+
+        public void Add(params IPrintingDevice[] devices)
+        {
+            foreach (var device in devices)
+            {
+                _devices.Add(device);
+            }
+        }
+        
+        public IEnumerator<IPrintingDevice> GetEnumerator()
+        {
+            return _devices.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ example8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using example8.Models;
+
+namespace example8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var printers = new Technics(new IPrintingDevice[]
+            {
+                new Printer(40),
+                new LaserPrinter(25),
+                new LaserPrinter(30)
+            });
+
+            
+            printers.Add(new LaserPrinter(223));
+            
+            foreach (var printer in printers)
+            {
+                printer.RefillCartridge();
+                printer.Print();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
